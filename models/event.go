b4673cd44455f21d559660f3f12ceb47286ea9bc
Siniuk Kirill <kirill.s.works@gmail.com>
2025-12-18 23:25:52 +0400

    add db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"rest/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -11,13 +14,33 @@ type Event struct {
 	UserId      int
 }
 
-var events = make([]Event, 0)
-
-func (event Event) Save() {
-	events = append(events, event)
-	// late add to DB
+func (event Event) Save() error {
+	insert := `INSERT INTO events (id, name, location, date_time, user_id)
+	VALUES(?, ?, ?, ?, ?)`
+	result, err := db.DB.Exec(insert, event.ID, event.Name, event.Location, event.DateTime, event.UserId)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	event.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	selectAll := `SELECT * FROM events`
+	rows, err := db.DB.Query(selectAll)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	events := make([]Event, 0)
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Location, &event.DateTime, &event.UserId)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
 }
